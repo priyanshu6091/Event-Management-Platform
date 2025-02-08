7a22d388c4bca97d0b84c5f730281d7266969295
priyanshu6091 <145902260+priyanshu6091@users.noreply.github.com>
2025-02-09 00:56:20 +0530

    index.js

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(
    cors({
       credentials: true,
-      origin: ["http://localhost:5173", "https://steady-choux-d05d47.netlify.app/"],
+      origin: ["http://localhost:5173", "https://steady-choux-d05d47.netlify.app/"]
    })
 );
 
